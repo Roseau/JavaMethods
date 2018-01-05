5cacf209ee30ccb9eb8a07381315cb7026fbf4e1
Roseau <asboyo@gmail.com>
2018-01-05 16:12:11 +0700

    minor changes

--- AplikasiMethod1.java
+++ AplikasiMethod1.java
@@ -1,5 +1,5 @@
 public class AplikasiMethod1{
-    public static void main(String[] args){
+    //public static void main(String[] args){
         AplikasiMethod2.showYourName("Brom", 78);
         AplikasiMethod2.luasPersegiPanjang(30,50);
         System.out.println("Memanggil Persegi panjang");
@@ -7,7 +7,7 @@ public class AplikasiMethod1{
         AplikasiMethod2.luasLingkaran(7);
         AplikasiMethod2.luasPersegi(10);
         System.out.println(AplikasiMethod2.Surprise());
-    }
+    //}
 }
 
 
