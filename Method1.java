e7831ebb970dd7cedb07df3a11c6e60f1e7e9deb
Roseau <asboyo@gmail.com>
2018-01-05 10:16:02 +0700

    menambahkan method berparameter

--- Method1.java
+++ Method1.java
@@ -9,7 +9,12 @@ public class Method1{
         System.out.println("Nama saya "+ Nama() + ", Umur saya "+ Umur());
     }
 
+    public static void sayMyname(String nama, int umur){
+        System.out.println("Nama saya adalah "+nama+". dan berumur "+umur);
+    }
+    
     public static void main(String[] args){
         Perkenalan();
+        sayMyname("Versailes",58);
     }
 }
