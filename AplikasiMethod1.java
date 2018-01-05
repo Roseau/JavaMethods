3a158fd80a0f8032ff3de03d1cc28aeb6f14b9ec
Roseau <asboyo@gmail.com>
2018-01-05 10:56:40 +0700

    menambahkan method baru

--- AplikasiMethod1.java
+++ AplikasiMethod1.java
@@ -1,6 +1,12 @@
 public class AplikasiMethod1{
     public static void main(String[] args){
         AplikasiMethod2.showYourName("Brom", 78);
+        AplikasiMethod2.luasPersegiPanjang(30,50);
+        System.out.println("Memanggil Persegi panjang");
+        AplikasiMethod2.tampilPersegi(5);
+        AplikasiMethod2.luasLingkaran(7);
+        AplikasiMethod2.luasPersegi(10);
+        System.out.println(AplikasiMethod2.Surprise());
     }
 }
 
--- AplikasiMethod2.java
+++ AplikasiMethod2.java
@@ -3,4 +3,28 @@ public class AplikasiMethod2{
         System.out.println("Nama : "+nama);
         System.out.println("Umur : "+umur);
     }
+    public static String Scare(){
+        return "BOOOOOOOOOO";
+    }
+    public static void luasPersegiPanjang(int panjang, int lebar){
+        System.out.println("Luas persegi panjang "+panjang*lebar);
+    }
+    public static void luasLingkaran(int jari){
+        System.out.println("Luas lingkaran dengan sisi "+jari+" adalah : "+jari*jari*3.14);
+    }
+    public static void luasPersegi(int sisi){
+        System.out.println("Luas dari persegi dengan sisi "+sisi+" adalah : "+sisi*sisi);
+    }
+    public static void tampilPersegi(int s){
+        for(int i=0;i<s;i++){
+            for(int j=0;j<s;j++){
+                System.out.print("#");
+            }
+            System.out.println("");
+        }
+    }
+    public static String Surprise(){
+        return Scare();
+    }
+
 }
